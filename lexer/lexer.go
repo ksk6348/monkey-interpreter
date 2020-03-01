d3b4e9ab65fc4d6aa06d7d67bec9602ee73e9d35
ohya <ksk6348@gmail.com>
2020-03-02 00:04:35 +0900

    string lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -82,6 +82,14 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '"':
+		l.readChar()
+		if isLetter(l.ch) {
+			tok.Type = token.STRING
+			tok.Literal = l.readString()
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -122,6 +130,14 @@ func isLetter(ch byte) bool {
 func (l *Lexer) readNumber() string {
 	position := l.position
 	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readString() string {
+	position := l.position
+	for l.ch != '"' && l.ch != 0 {
 		l.readChar()
 	}
 	return l.input[position:l.position]
